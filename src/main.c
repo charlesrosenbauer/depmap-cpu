63a65d890a3031e2983a38f3e0452cd9a17b6287
Charles <charles.rosenbauer@gmail.com>
2022-12-17 23:58:47 -0600

    Some notes about next steps

--- src/main.c
+++ src/main.c
@@ -3,6 +3,23 @@
 #include "cpu.h"
 
 
-int main(){
+/*
+	NOTES:
+	
+	* I have a different project in a private repo that I'm building a core
+	  simulator and assembler for. I can probably just port that to this with a
+	  few changes.
+	* Keep RAM and register count small for easier dependency tracking.
+	* Drop false dependencies (WAW, etc.).
+	* Generate dependency graph (export if requested).
+	* Have several mechanisms for plotting dependency graph.
+	  * time-oriented
+	  * color-based clustering
+	  * position-based clustering
+	  * etc.
+*/
+
+
+int main(int ac, char** av){
 	printf("Hello world!\n");
 }
